f722d584dddfb932e9e813f4c68cb288e1af5e1f
Noviana Gresita <90511661+novianagrsta@users.noreply.github.com>
2023-10-28 20:25:38 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,87 @@
-import { Button } from "react-bootstrap"
-import './App.css'
-import ProductCard from "./components/ProductCard"
-import { useEffect, useState } from "react"
-import getProducts from "./services/getProduct"
-import LoadingSpinner from "./components/LoadingSpinner"
-import AddProduct from "./components/AddProduct"
-import EditProduct from "./components/EditProduct"
-
+import React from 'react';
+import './App.css';
+import ProductCard from './components/ProductCard';
+import { useEffect, useState } from 'react';
+import getProducts from './services/getProducts';
+import LoadingSpinner from './components/LoadingSpinner';
+import AddProduct from './components/AddProduct';
+import EditProduct from './components/EditProduct';
+import { Button } from 'react-bootstrap';
 
 function App() {
-  const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [showAddProduct, setShowAddProduct] = useState(false)
-  const [showEditProduct, setShowEditProduct] = useState(false)
-  const [data, setData] = useState(null)
-
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showAddProduct, setShowAddProduct] = useState(false);
+  const [showEditProduct, setShowEditProduct] = useState(false);
+  const [data, setData] = useState(null);
 
   const fetchProducts = async () => {
-    setLoading(true)
+    setLoading(true);
     const data = await getProducts();
-    setProducts(data)
-    setLoading(false)
-  }
+    setProducts(data);
+    setLoading(false);
+  };
 
   const handleClose = () => {
-    setShowAddProduct(false)
-    setShowEditProduct(false)
-  }
+    setShowAddProduct(false);
+    setShowEditProduct(false);
+  };
 
   const onCreate = () => {
-    fetchProducts()
-    handleClose()
-  }
+    fetchProducts();
+    handleClose();
+  };
 
   const onUpdate = () => {
-    fetchProducts()
-    handleClose()
-  }
+    fetchProducts();
+    handleClose();
+  };
 
   const handleSetData = (data) => {
-    setData(data)
-    setShowEditProduct(true)
-  }
+    setData(data);
+    setShowEditProduct(true);
+  };
 
   useEffect(() => {
     fetchProducts();
-  }, [])
+  }, []);
 
   return (
-    <div className="container">
-      <div className="d-flex justify-content-between mt-5">
-        <h1>List Product</h1>
-        <Button variant="primary"
-          onClick={() => setShowAddProduct(true)}
-        >
-          Tambah Product
-        </Button>
+    <div>
+      <header class="header">
+        <img src="https://smglobalshop.com/cdn/shop/files/SGS_LOGO_ONLY.png?v=1681320954" alt="Logo" width="100" height="100" />
+        <h1>My Product List</h1>
+      </header>
+
+
+      <div className="container">
+        <div className="d-flex justify-content-end mt-5">
+          <Button variant="primary" onClick={() => setShowAddProduct(true)}>Tambah Product</Button>
+        </div>
+
+        <div className="product-list mt-5">
+          {loading && <LoadingSpinner />}
+          {!loading && products.map((product) => {
+            return (
+              <ProductCard
+                product={product}
+                key={product.id}
+                fetchProducts={fetchProducts}
+                handleSetData={() => handleSetData(product)}
+              />
+            );
+          })}
+        </div>
+
+        <AddProduct show={showAddProduct} handleClose={handleClose} onCreate={onCreate} />
+        <EditProduct show={showEditProduct} handleClose={handleClose} onUpdate={onUpdate} data={data} />
       </div>
-      <div className="product-list mt-5">
-        {loading && <LoadingSpinner />}
-        {!loading && products.map(product => {
-          return <ProductCard product={product} key={product.id} fetchProducts={fetchProducts} handleSetData={() => handleSetData(product)} />
-        })}
-      </div>
-      <AddProduct show={showAddProduct} handleClose={handleClose} onCreate={onCreate} />
-      <EditProduct show={showEditProduct} handleClose={handleClose} onUpdate={onUpdate} data={data} />
+
+      <footer className="footer">
+        <p>Copyright &copy; 2023 Noviana Gresita Br. Perangin-Angin</p>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
