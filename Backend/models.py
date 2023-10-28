61cd1ce790755968afeefa9056e593a1c7455346
Noviana Gresita <90511661+novianagrsta@users.noreply.github.com>
2023-10-28 19:18:04 +0700

    Create models.py

---
+++ Backend/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.Text)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f"<Product {self.name}>"
