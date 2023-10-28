d84884a5a91592bac2c592fe0d7765e152103d24
Noviana Gresita <90511661+novianagrsta@users.noreply.github.com>
2023-10-28 20:27:05 +0700

    Update deleteProduct.js

--- src/services/deleteProduct.js
+++ src/services/deleteProduct.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const url = 'https://653b817b2e42fd0d54d539c2.mockapi.io/api/v1/product';
+const url = 'https://653b817d2e42fd0d54d539d4.mockapi.io/api/v1/product';
 
-export default async function deleteProduct(id) {
+export default async function deleteProducts(id) {
   try {
     const response = await axios.delete(`${url}/${id}`);
     return response.data;
